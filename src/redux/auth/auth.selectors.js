5ef8d77ce428ade79a6b97fd01d13672c906aaf8
bmv000 <106543741+bmv000@users.noreply.github.com>
2023-04-22 10:07:16 +0300

    Додада селектори в auth.selectors

--- src/redux/auth/auth.selectors.js
+++ src/redux/auth/auth.selectors.js
@@ -1,2 +1,6 @@
 // export const selectAuthToken = state => state.auth.data;
-export const selectAuthToken = () => true;
+// export const selectAuthToken = () => true;
+export const selectIsLogin = state => state.auth.isLoggedIn;
+export const selectUserName = state => state.auth.user.name;
+export const selectUserEmail = state => state.auth.user.email;
+export const selectToken = state => state.auth.token;
