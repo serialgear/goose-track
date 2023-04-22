a64dd3ce104bb68718382a03dd285ec691a39447
Dmytro Muzyka <mmitya889@gmail.com>
2023-04-22 23:34:37 +0300

    fixed styles import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './styles/common.css';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
