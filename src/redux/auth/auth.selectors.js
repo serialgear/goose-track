c3a896a128da2b475c2419260a864b28f1c5b814
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-04-23 20:51:32 +0300

    updated MainLayout

--- src/redux/auth/auth.selectors.js
+++ src/redux/auth/auth.selectors.js
@@ -1,5 +1,5 @@
 // export const selectAuthToken = state => state.auth.token;
-export const selectAuthToken = () => false;
+export const selectAuthToken = () => true;
 export const selectIsLogin = state => state.auth.isLoggedIn;
 export const selectUserName = state => state.auth.user.name;
 export const selectUserEmail = state => state.auth.user.email;
