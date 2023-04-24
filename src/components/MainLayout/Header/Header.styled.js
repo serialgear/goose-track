3dd0e966885514b877849c9ae119be830de488ca
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-04-24 16:09:03 +0300

    started header

--- src/components/MainLayout/Header/Header.styled.js
+++ src/components/MainLayout/Header/Header.styled.js
@@ -2,11 +2,46 @@ import styled from 'styled-components';
 
 export const HeaderStyled = styled.header`
   display: flex;
+  align-items: center;
   justify-content: space-between;
   max-width: 100%;
+  /* padding-left: 20px;
+  padding-right: 20px; */
+  padding-top: 24px;
+  margin-bottom: 64px;
 `;
 
 export const HeaderWrapper = styled.div`
   display: flex;
-  gap: 10px;
+  align-items: center;
+  gap: 16px;
+`;
+
+// export const MobileMenu = styled.button`
+// display: flex;
+// @media (min-width: 1280px) {
+//     display: none;
+//   }
+
+// `
+export const PageName = styled.h2`
+  display: none;
+  @media (min-width: 1280px) {
+    display: flex;
+  }
+`;
+
+export const MobileMenuSvg = styled.svg`
+  width: 34px;
+  height: 34px;
+  fill: var(--primary-text-color);
+  display: flex;
+  @media (min-width: 1280px) {
+    display: none;
+  }
+
+  /* @media screen and (min-width: 768px) {
+    width: 44px;
+    height: 44px;
+  } */
 `;
