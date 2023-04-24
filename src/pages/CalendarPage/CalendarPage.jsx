92d1cc8af83ffebe06c51f3c78c9cb3f8712f49c
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-04-24 23:38:56 +0300

    Update CalendarPage.jsx

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,13 +1,11 @@
 import { CalendarToolbar } from "components/CalendarToolbar/CalendarToolbar";
 import { Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
 import { Container } from "./CalendarPage.styled";
 import { Outlet } from "react-router";
 import { parseISO } from "date-fns";
 import { useSelector } from "react-redux";
 import { selectCurrentMonth } from "redux/calendar/calendar.selectors";
 import { selectUserName } from "redux/auth/auth.selectors";
-import { replace } from "formik";
 
  export default function CalendarPage() {
   const currentDate = parseISO(useSelector(selectCurrentMonth));
