a1813771fadc665070c922e033948ef3d9e83682
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-04-25 16:02:25 +0300

    header

--- src/components/MainLayout/Header/Header.jsx
+++ src/components/MainLayout/Header/Header.jsx
@@ -1,30 +1,34 @@
 import { ThemeToggler } from 'components/ThemeToggler/ThemeToggler';
 import {
+  AvatarImg,
+  AvatarSvg,
   HeaderStyled,
   HeaderWrapper,
   MobileMenuSvg,
   PageName,
+  UserName,
 } from './Header.styled';
 import { useSelector } from 'react-redux';
-import { selectUserName } from 'redux/auth/auth.selectors';
-// import { useLocation } from 'react-router-dom';
+import { selectUserAvatarURL, selectUserName } from 'redux/auth/auth.selectors';
+import { useLocation } from 'react-router-dom';
 import icon from '../../../images/sprite.svg';
 
 export const Header = ({ openMobalMenu }) => {
-  // const dispath = useDispatch();
   const name = useSelector(selectUserName);
+  const avatarUrl = useSelector(selectUserAvatarURL);
 
-  // const location = useLocation();
+  const location = useLocation();
+  let isActivePage = location.pathname.split('/')[1];
 
-  // console.log(location.state);
-
-  // const handlLogOut =()=>{
-  //     dispath(logOut())
-  // }
+  if (isActivePage === 'calendar') {
+    isActivePage = 'Calendar';
+  } else if (isActivePage === 'account') {
+    isActivePage = 'User Profile';
+  }
 
   return (
     <HeaderStyled>
-      {<PageName>{}PageName</PageName>}
+      {<PageName>{isActivePage}</PageName>}
 
       <MobileMenuSvg
         onClick={() => openMobalMenu(true)}
@@ -36,19 +40,21 @@ export const Header = ({ openMobalMenu }) => {
       <HeaderWrapper>
         <ThemeToggler></ThemeToggler>
 
-        <h2>{name.split(' ')[0]}</h2>
+        <UserName>{name.split(' ')[0]}</UserName>
 
-        <button
-          type="button"
-          //   onClick={handleUserSmallInfo}
-        >
-          <img
-            width="44"
-            height="44"
-            src="https://img.icons8.com/arcade/64/null/rick-sanchez.png"
-            alt="user"
-          />
-        </button>
+        {avatarUrl ? (
+          <button
+          //   onClick={openUserSmallInfo}
+          >
+            <AvatarImg src={avatarUrl} alt="user" />
+          </button>
+        ) : (
+          <AvatarSvg
+          //    onClick={openUserSmallInfo}
+          >
+            <use xlinkHref={`${icon}#profile-avatar-f`}></use>
+          </AvatarSvg>
+        )}
       </HeaderWrapper>
     </HeaderStyled>
   );
