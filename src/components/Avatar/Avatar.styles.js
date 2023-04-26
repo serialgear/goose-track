53433b711328d75bffb1b0a8fa6447616c7bfee7
Dmytro Muzyka <mmitya889@gmail.com>
2023-04-26 12:18:04 +0300

    fixed avatar styles

--- src/components/Avatar/Avatar.styles.js
+++ src/components/Avatar/Avatar.styles.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const AvatarSvg = styled.svg`
   width: inherit;
   height: inherit;
-  fill: var(--auth-bg-color);
+  fill: var(--avatar-fill-color);
+  background-color: var(--avatar-background-color);
   border-radius: 50%;
   border: solid 1px var(--accent-background-color);
-  /* fill-opacity: 0.18; */
 `;
 
 export const AvatarImg = styled.img`
