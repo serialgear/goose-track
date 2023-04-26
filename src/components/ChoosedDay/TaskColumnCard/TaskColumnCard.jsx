f62954e20535e5b729518da6e4a60ffde3a1e645
Dmytro Muzyka <mmitya889@gmail.com>
2023-04-26 12:20:31 +0300

    update TaskColumnCard

--- src/components/ChoosedDay/TaskColumnCard/TaskColumnCard.jsx
+++ src/components/ChoosedDay/TaskColumnCard/TaskColumnCard.jsx
@@ -1,16 +1,31 @@
-import { Avatar } from "components/MainLayout/Avatar/Avatar"
-import { Container, TaskAvatarWrapper, TaskTitle, Wrapper } from "./TaskColumnCard.styled"
-// import { AvatarWrapper } from "components/MainLayout/Avatar/Avatar.styles"
+import { Avatar } from 'components/Avatar/Avatar';
+import {
+  Container,
+  TaskPriority,
+  TaskAvatarWrapper,
+  TaskTitle,
+  Wrapper,
+} from './TaskColumnCard.styled';
+import { TaskToolbar } from '../TaskToolbar/TaskToolbar';
+import { TASK_PRIORITY } from 'constants/taskPriority.constants';
 
-export const TaskColumnCard = () =>{
-    return <Container>
-        <TaskTitle>Brainstorm ideas for new content or products</TaskTitle>
+export const TaskColumnCard = ({
+  title = 'Lorem ipsum dolor sit amet consectetur ',
+  priority = TASK_PRIORITY.low,
+  ...other
+}) => {
+  return (
+    <Container>
+      <TaskTitle>{title}</TaskTitle>
+      <Wrapper>
         <Wrapper>
-            <TaskAvatarWrapper>
-
-            <Avatar/>
-</TaskAvatarWrapper>
-            <div>TaskToolbar</div>
+          <TaskAvatarWrapper>
+            <Avatar />
+          </TaskAvatarWrapper>
+          <TaskPriority priority={priority}>{priority}</TaskPriority>
         </Wrapper>
+        <TaskToolbar {...other} />
+      </Wrapper>
     </Container>
-}
+  );
+};
--- src/components/ChoosedDay/TaskColumnCard/TaskColumnCard.styled.js
+++ src/components/ChoosedDay/TaskColumnCard/TaskColumnCard.styled.js
@@ -1,10 +1,13 @@
-import styled from "styled-components";
+import { TASK_PRIORITY } from 'constants/taskPriority.constants';
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  display  : flex;
+  display: flex;
   flex-direction: column;
   gap: 28px;
   padding: 15px;
+
+  max-width: 300px;
 
   background-color: var(--secondary-background-color);
   border: var(--border);
@@ -12,24 +15,47 @@ export const Container = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  display  : flex;
+  display: flex;
   justify-content: space-between;
+  align-items: flex-end;
+  gap: 8px;
 `;
 
-export const TaskTitle = styled.h3`
-font-style: normal;
-font-weight: 500;
-font-size: 14px;
-line-height: 18px;
-/* or 129% */
-
-
-color: #111111;
+export const TaskTitle = styled.h4`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.29;
 `;
 
 export const TaskAvatarWrapper = styled.div`
   width: 32px;
   height: 32px;
+`;
+
+export const TaskPriority = styled.p`
+  padding: 4px 12px;
+  background: var(
+    ${props => {
+      if (props.priority === TASK_PRIORITY.medium) {
+        return '--task-priority-medium-color';
+      }
+      if (props.priority === TASK_PRIORITY.high) {
+        return '--task-priority-high-color';
+      }
+      return '--task-priority-low-color';
+    }}
+  );
+  border-radius: 4px;
+
+  font-style: normal;
+  font-weight: 600;
+  font-size: 10px;
+  line-height: 12px;
+  /* identical to box height, or 120% */
+
+  text-align: center;
+
+  color: var(--btn-text-color);
 
   /* fill-opacity: 0.18; */
   /* overflow: hidden; */
