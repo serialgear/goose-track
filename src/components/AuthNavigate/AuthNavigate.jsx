f94f8448e7872214a23c37e773eb8e97fa1117c7
Dmytro Muzyka <mmitya889@gmail.com>
2023-04-26 17:32:23 +0300

    Update AuthNavigate.jsx

--- src/components/AuthNavigate/AuthNavigate.jsx
+++ src/components/AuthNavigate/AuthNavigate.jsx
@@ -1,17 +1,16 @@
-import LoginPage from 'pages/LoginPage/LoginPage';
-import RegisterPage from 'pages/RegisterPage/RegisterPage';
-import { Fragment } from 'react';
-// import { NavLink } from 'react-router-dom';
+// import LoginPage from 'pages/LoginPage/LoginPage';
+// import RegisterPage from 'pages/RegisterPage/RegisterPage';
+// import { Fragment } from 'react';
+// // import { NavLink } from 'react-router-dom';
 
-
-export const AuthNav = () => {
-  return (
-    <Fragment>
-      <RegisterPage/>
-      <LoginPage/>
-      {/* <NavLink  to="/login">
-        Log In
-      </NavLink> */}
-    </Fragment>
-  );
-};
+// export const AuthNav = () => {
+//   return (
+//     <Fragment>
+//       <RegisterPage/>
+//       <LoginPage/>
+//       {/* <NavLink  to="/login">
+//         Log In
+//       </NavLink> */}
+//     </Fragment>
+//   );
+// };
