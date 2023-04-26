d0fd86b25a9af0623d20a658225c1b74d6399115
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-04-26 15:42:04 +0300

    header

---
+++ src/components/AuthNavigate/AuthNavigate.jsx
@@ -0,0 +1,17 @@
+import LoginPage from 'pages/LoginPage/LoginPage';
+import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import { Fragment } from 'react';
+// import { NavLink } from 'react-router-dom';
+
+
+export const AuthNav = () => {
+  return (
+    <Fragment>
+      <RegisterPage/>
+      <LoginPage/>
+      {/* <NavLink  to="/login">
+        Log In
+      </NavLink> */}
+    </Fragment>
+  );
+};
