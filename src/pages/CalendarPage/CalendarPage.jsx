6dc2050dc3b9185be58c6b9fba0d27c4d12507f0
Dmytro Muzyka <mmitya889@gmail.com>
2023-04-27 10:09:47 +0300

    Update CalendarPage.jsx

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,24 +1,21 @@
-import { CalendarToolbar } from "components/CalendarToolbar/CalendarToolbar";
-import { Container } from "./CalendarPage.styled";
-import { Outlet } from "react-router";
-import { TaskColumnCard } from "components/ChoosedDay/TaskColumnCard/TaskColumnCard";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getTasksOfMonth } from "redux/calendar/calendar.operations";
+import { CalendarToolbar } from 'components/CalendarToolbar/CalendarToolbar';
+import { Container } from './CalendarPage.styled';
+import { Outlet } from 'react-router';
+import { TaskColumnCard } from 'components/ChoosedDay/TaskColumnCard/TaskColumnCard';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+// import { getTasksOfMonth } from "redux/calendar/calendar.operations";
 
+export default function CalendarPage() {
+  const dispatch = useDispatch();
 
- export default function CalendarPage() {
- const dispatch = useDispatch();
+  useEffect(() => {}, [dispatch]);
 
-  useEffect(()=> {
-
-  }, [dispatch])
-  
   return (
     <Container>
       <CalendarToolbar />
       <Outlet />
-      <TaskColumnCard/>
+      <TaskColumnCard />
     </Container>
   );
 }
