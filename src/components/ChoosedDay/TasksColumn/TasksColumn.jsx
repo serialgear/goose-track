616f826b2f89e5e363145a66ab45ef39936b2d54
Dmytro Muzyka <mmitya889@gmail.com>
2023-04-28 16:40:28 +0300

    update TasksColumn

--- src/components/ChoosedDay/TasksColumn/TasksColumn.jsx
+++ src/components/ChoosedDay/TasksColumn/TasksColumn.jsx
@@ -1,15 +1,16 @@
-import { AddTaskBtn } from "../AddTaskBtn/AddTaskBtn"
-import { ColumnHeadBar } from "../ColumnHeadBar/ColumnHeadBar"
-import { Container } from "./TasksColumn.styled"
+import { AddTaskBtn } from '../AddTaskBtn/AddTaskBtn';
+import { ColumnHeadBar } from './ColumnHeadBar/ColumnHeadBar';
+import { ColumnsTasksList } from './ColumnsTasksList/ColumnsTasksList';
+import { Container } from './TasksColumn.styled';
 
-export const TasksColumn = () => {
-
-    return(
-       <>
-        <Container>
-        <ColumnHeadBar />
-        <AddTaskBtn/>
-        </Container>
-       </>
-    )
-}
+export const TasksColumn = ({ status }) => {
+  return (
+    <>
+      <Container>
+        <ColumnHeadBar title={status.name} />
+        <ColumnsTasksList status={status} />
+        <AddTaskBtn />
+      </Container>
+    </>
+  );
+};
