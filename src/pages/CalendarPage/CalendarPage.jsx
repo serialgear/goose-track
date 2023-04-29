4171735577d1ac20dccac3aeeafc8bf9ee215e24
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-04-30 00:17:50 +0300

    update refreshUser

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -6,20 +6,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentMonth } from 'redux/calendar/calendar.selectors';
 import { getTasksOfMonth } from 'redux/calendar/calendar.operations';
 import { format } from 'date-fns';
-import { toast } from 'react-toastify';
-import { refreshUser } from 'redux/auth/auth.operations';
 
 export default function CalendarPage() {
   const dispatch = useDispatch();
   const currentDate = useSelector(selectCurrentMonth);
-
-  useEffect(() => {
-    try {
-      dispatch(refreshUser());
-    } catch (error) {
-      toast.error('Authorization error');
-    }
-  }, [dispatch]);
 
   useEffect(() => {
     dispatch(
