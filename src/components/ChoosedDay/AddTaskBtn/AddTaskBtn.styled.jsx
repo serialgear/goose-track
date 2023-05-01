e4ad762b30a826f7c8b1bcb16949784329878205
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2023-05-02 02:32:05 +0300

    Update AddTaskBtn.styled.jsx

--- src/components/ChoosedDay/AddTaskBtn/AddTaskBtn.styled.jsx
+++ src/components/ChoosedDay/AddTaskBtn/AddTaskBtn.styled.jsx
@@ -1,33 +1,42 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Button = styled.button`
-display: flex;
-justify-content: center;
-align-items: center;
-padding-top: 16px;
-padding-bottom: 16px;
-gap: 8px;
-isolation: isolate;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  gap: 8px;
+  isolation: isolate;
 
-width: 100%;
-height: 48px;
+  width: 100%;
+  height: 48px;
 
-background: var(--active-item-sidebar-background);
-border: var(--border-AddTaskBtn);
-border-radius: 8px;
+  background-color: var(--active-item-sidebar-background);
+  border: var(--border-AddTaskBtn);
+  border-radius: 8px;
+
+  transition: background-color var(--animation), box-shadow var(--animation);
+  &:hover,
+  :focus {
+    box-shadow: 4px 2px 16px rgba(136, 165, 191, 1);
+    background-color: var(--hover-btn-background-color);
+  }
 `;
- export const AddTask = styled.span`
- font-family: var(--primary-font);
-font-style: normal;
-font-weight: 600;
-font-size: 14px;
-line-height: 1.29;
 
-color: var(--primary-text-color);
- `;
- export const Plus = styled(AddTask)`
- margin-bottom: 4px;
+export const AddTask = styled.span`
+  font-family: var(--primary-font);
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 1.29;
 
-font-weight: 300;
-font-size: 32px;
- `;
+  color: var(--primary-text-color);
+`;
+
+export const Plus = styled(AddTask)`
+  margin-bottom: 4px;
+
+  font-weight: 300;
+  font-size: 32px;
+`;
