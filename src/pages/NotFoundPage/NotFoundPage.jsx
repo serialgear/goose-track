fcea06b98515988920617b7a2a89b5716ec44ffb
Bzhezinskyi <bzhezinskyiillia@gmail.com>
2023-05-01 23:55:15 +0300

    create NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,17 @@
-import { Container } from 'components/MainLayout/MainLayout.styled';
+import * as STC from './NotFoundPage.styled';
+import Img1 from '../../images/NotFoundPage/NotFoundPage1.png';
+import Img2 from '../../images/NotFoundPage/NotFoundPage2.png';
 
 export default function NotFoundPage() {
   return (
-    <Container>
-      <h1>NotFoundPage</h1>
-    </Container>
+    <STC.Container>
+      <STC.Img src={Img2} className="left" />
+      <STC.Box>
+        <STC.Title>404</STC.Title>
+        <STC.Txt>Page not found</STC.Txt>
+        <STC.BtnHome to="/">Go to home</STC.BtnHome>
+      </STC.Box>
+      <STC.Img src={Img1} className="right" />
+    </STC.Container>
   );
 }
