c883c99ced2f467d2cb018f0e4ff68760ce9b1ce
Vitalii Berko <vitalii.v.berko@gmail.com>
2023-05-02 10:53:58 +0300

    Added style for delete modal

--- src/components/TaskDelete/TaskDelete.jsx
+++ src/components/TaskDelete/TaskDelete.jsx
@@ -1,25 +1,26 @@
 import { useDispatch } from 'react-redux';
 import { Modal } from '../Modal/Modal';
 import { deleteTaskOperation } from 'redux/calendar/calendar.operations';
-import { Thumb } from './TaskDelete.styled';
+import { ButtonCancel, ButtonDel, Text, Wrapper } from './TaskDelete.styled';
 
+export const TaskDelete = ({ handleCloseDeleteModal, id }) => {
+  const dispatch = useDispatch();
 
-
-export const TaskDelete = ({handleCloseDeleteModal, id }) => {
-    const dispatch = useDispatch();
-
-    const handleDelete = () => {
-        dispatch(deleteTaskOperation(id));
-      };
+  const handleDelete = () => {
+    dispatch(deleteTaskOperation(id));
+  };
   return (
     <>
-      <Modal 
-      onClose={handleCloseDeleteModal}
-      >
-        <p> Do you really want to delete your task?</p>
-        <Thumb><button type='button' onClick={handleDelete}>Yes</button>
-        <button type='button' onClick={()=>handleCloseDeleteModal()}>Cancel</button></Thumb>
-        
+      <Modal onClose={handleCloseDeleteModal}>
+        <Text> Do you really want to delete your task?</Text>
+        <Wrapper>
+          <ButtonDel type="button" onClick={handleDelete}>
+            Yes
+          </ButtonDel>
+          <ButtonCancel type="button" onClick={() => handleCloseDeleteModal()}>
+            Cancel
+          </ButtonCancel>
+        </Wrapper>
       </Modal>
     </>
   );
--- src/components/TaskDelete/TaskDelete.styled.js
+++ src/components/TaskDelete/TaskDelete.styled.js
@@ -1,9 +1,82 @@
 import styled from "styled-components";
 
 
-export const Thumb = styled.div`
+export const Wrapper = styled.div`
 display: flex;
 align-items: center;
 justify-content: space-around;
-padding: 8px;
+padding: 16px;
 `
+export const Text = styled.h2`
+  font-family: var(--primary-font);
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 18px;
+  text-align: center;
+`;
+
+export const ButtonDel = styled.button`
+  width: 30%;
+  height: 42px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 12px;
+  padding: 14px;
+  background-color: var(--accent-background-color);
+  box-shadow: 4px 2px 16px rgba(136, 165, 191, 0.48);
+  border-radius: 8px;
+  border: none;
+  color: var(--btn-text-color);
+  font-family: var(--primary-font);
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: -0.02em;
+
+  transition: box-shadow var(--animation), background-color var(--animation);
+  &:hover,
+  :focus {
+    box-shadow: 4px 2px 16px rgba(136, 165, 191, 1);
+    background-color: var(--hover-btn-background-color);
+  }
+
+  @media (min-width: 768px) {
+    /* width: 400px; */
+    height: 48px;
+  }
+`;
+
+export const ButtonCancel = styled.button`
+  /* display: flex;
+  align-items: center;
+  justify-content: center; */
+  width: 30%;
+  height: 42px;
+  padding: 12px;
+  column-gap: 12px;
+  background: var(--task-btn-cancel-bg-color);
+  border-radius: 8px;
+  border: none;
+  text-align: center;
+  font-family: var(--primary-font);
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: -0.02em;
+
+  transition: box-shadow var(--animation);
+  &:hover,
+  :focus {
+    box-shadow: 4px 2px 16px rgba(136, 165, 191, 1);
+  }
+
+  @media (min-width: 768px) {
+    /* width: 400px; */
+    height: 48px;
+  }
+`;
+
