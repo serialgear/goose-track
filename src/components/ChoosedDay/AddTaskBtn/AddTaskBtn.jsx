e408dfa4160792dca398071bbec346d9e9747c99
Владимир Перетятько <djbob2000@gmail.com>
2023-05-02 21:11:23 +0300

    fix add on plus button

--- src/components/ChoosedDay/AddTaskBtn/AddTaskBtn.jsx
+++ src/components/ChoosedDay/AddTaskBtn/AddTaskBtn.jsx
@@ -1,28 +1,12 @@
 import { Button, AddTask, Plus } from './AddTaskBtn.styled';
-import { TaskModal } from '../../TaskModal/TaskModal';
-import { useState } from 'react';
 
-export const AddTaskBtn = ({ status }) => {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
+export const AddTaskBtn = ({ status, handleShowModal }) => {
   return (
     <>
       <Button type="button" onClick={handleShowModal}>
         <Plus>+</Plus>
         <AddTask>Add task</AddTask>
       </Button>
-
-      {showModal && (
-        <TaskModal handleCloseModal={handleCloseModal} status={status} />
-      )}
     </>
   );
 };
--- src/components/ChoosedDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.jsx
+++ src/components/ChoosedDay/TasksColumn/ColumnHeadBar/ColumnHeadBar.jsx
@@ -1,11 +1,11 @@
 import { Wrapper, Title, Button, Icon } from './ColumnHeadBar.styled';
 import Icons from 'images/sprite.svg';
 
-export const ColumnHeadBar = ({ title }) => {
+export const ColumnHeadBar = ({ title, handleShowModal }) => {
   return (
     <Wrapper>
       <Title>{title}</Title>
-      <Button type="button">
+      <Button type="button" onClick={handleShowModal}>
         <Icon>
           <use href={`${Icons}#profile-plus-s`}></use>
         </Icon>
